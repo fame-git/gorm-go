bd211b2ec6b6f0cded69073a2e14533fcfa41b40
fame-git <u7444134@anu.edu.au>
2024-02-18 16:40:25 +0700

    update some

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 const (
@@ -19,7 +23,19 @@ func main() {
 	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// logger for query inspection
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second, // Slow SQL threshold
+			LogLevel:      logger.Info, // Log level
+			Colorful:      true,        // Enable color
+		},
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
 
 	if err != nil {
 		panic("failed to connect to database")
@@ -27,4 +43,20 @@ func main() {
 
 	db.AutoMigrate(&Book{})
 	fmt.Println("Database migration completed!")
+
+	//deleteBook(db, 1)
+
+	/* createBook(db, &Book{
+		Name:        "die",
+		Author:      "die-queen",
+		Price:       400,
+		Description: "Super die",
+	}) */
+
+	currentBook := searchBook(db, "die")
+
+	for _, book := range currentBook {
+		fmt.Println(book.ID, book.Name, book.Author, book.Price)
+	}
+
 }
--- models.go
+++ models.go
@@ -1,9 +1,69 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/gorm"
+)
+
 type Book struct {
-	ID          int64
+	gorm.Model
 	Name        string
 	Author      string
 	Description string
-	price       uint
+	Price       uint
+}
+
+func createBook(db *gorm.DB, book *Book) {
+	result := db.Create(book)
+
+	if result.Error != nil {
+		log.Fatalf("Error creating book: %v", result.Error)
+	}
+
+	fmt.Println("Create Book Successful")
+}
+
+func getBook(db *gorm.DB, id uint) *Book {
+	var book Book
+	result := db.First(&book, id)
+
+	if result.Error != nil {
+		log.Fatalf("Error get book: %v", result.Error)
+	}
+
+	return &book
+}
+
+func updateBook(db *gorm.DB, book *Book) {
+	result := db.Save(book)
+
+	if result.Error != nil {
+		log.Fatalf("Update book failed: %v", result.Error)
+	}
+
+	fmt.Println("Update book successful")
+}
+
+func deleteBook(db *gorm.DB, id uint) {
+	var book Book
+	result := db.Delete(&book, id)
+
+	if result.Error != nil {
+		log.Fatalf("Delete book failed: %v", result.Error)
+	}
+
+	fmt.Println("Delete book successful")
+}
+
+func searchBook(db *gorm.DB, bookName string) []Book {
+	var books []Book
+
+	result := db.Where("name = ?", bookName).Order("price desc").Find(&books)
+	if result.Error != nil {
+		log.Fatalf("Search book failed: %v", result.Error)
+	}
+
+	return books
 }
